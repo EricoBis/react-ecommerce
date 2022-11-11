4204f0ea27d58431cd4c1755e110546ad33ba0d6
Érico Bis <ericobis.santos@gmail.com>
2022-11-11 10:39:55 -0300

    changed name of const "image"

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -7,11 +7,11 @@ import {Img} from "../style";
 
 function ProductCard({ value }) {
   const {addToCart} = useContext(CartContext);
-  const image = `img/${value.imgUrl}`
+  const url = `img/${value.imgUrl}`
 
   return (
     <Card>
-      <Img src={image} alt=""/>
+      <Img src={url} alt=""/>
       <ProductTitle className="product-title">{value.title}</ProductTitle>
       <h3 className="produt-price">R${value.price}</h3>
       <CartButton onClick={()=> addToCart(value)}><BsCart4/></CartButton>
